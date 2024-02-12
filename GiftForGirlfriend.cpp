93d5bb3537ab25685d279f28b3e5804081ced350
Taha Burak Sahin <thbrkshn@gmail.com>
2024-02-12 15:32:55 +0100

    Add files via upload
    "A heartfelt journey of love unfolds through this C++ program, where loading messages and ASCII art combine to express deep emotions, crafted with passion.

---
+++ GiftForGirlfriend.cpp
@@ -0,0 +1,299 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for (i = 0; i <= 100; i++)
+    {
+        system("cls");
+        cout << "\n\t\t\t Connecting to Love Server...";
+        cout << "\n\n\t\t\t...::... is sending to my girlfriend !";
+        cout << "\n\n\t\t__________________________________________";
+        cout << "\n\n\n\t\t\t\t Loading   " << i << " %"<<endl;
+        if (i == 10)
+        {
+            cout << "\n\n\t\t\tT            ";
+            cout << "\n\n\n\t\t\"I HAVE SOMETHING TO SHARE WITH YOU. PLEASE LISTEN!!!\"   ";
+            Sleep(4000);
+        }
+        if (i == 20)
+        {
+            cout << "\n\n\n\t\tLoading....";
+            Sleep(3000);
+        }
+        if (i == 30)
+        {
+            cout << "\n\n\t\t\tI                    ";
+            cout << "\n\n\n\t\t\"Since the day I met you, there hasn't been a day without a smile on my face.\" \n\t\t\"I felt like we've met before, I think!\"";
+            Sleep(4000);
+        }
+        if (i == 40)
+        {
+            cout << "\n\n\t\t\tI L                 ";
+            cout << "\n\n\n\t\t\"Since I saw you, my heart has always started beating faster.\" \n\t\t\"I think you stole my heart!\"";
+            Sleep(4000);
+        }
+        if (i == 50)
+        {
+            cout << "\n\n\t\t\tI LO               ";
+            cout << "\n\n\n\t\t\"Do you know? I'm always thinking about you!\" \n\t\tYou are my baby.";
+            Sleep(5000);
+        }
+        if (i == 60)
+        {
+            cout << "\n\n\t\t\tI LOVE            ";
+            cout << "\n\n\n\t\t\"There are 7 billion people in the world.\" \n\t\t\"But there's only one person who loves you.\"";
+            cout << "\n\t\t==>AND THAT IS MEEE !!!";
+            Sleep(5000);
+        }
+        if (i == 70)
+        {
+            cout << "\n\n\t\t\tI     LOVE    Y       ";
+            cout << "\n\n\n\t\t\"I'm a technical person, not very romantic or poetic.\" \n\t\t\"But when I look into your eyes, equations fade away, and all I see is the poetry of love.\"";
+            Sleep(4000);
+        }
+        if (i == 80)
+        {
+            cout << "\n\n\t\t\tI     LOVE    YO       ";
+            cout << "\n\n\n\t\t\"I learned object-oriented programming.\"\n\t\t\"However,the object I aim for is you!\"  \n";
+            Sleep(4000);
+        }
+        if (i == 90)
+        {
+            cout << "\n\n\t\t\tI    LOVE    YOU       ";
+            cout << "\n\n\n\t\tBE MY LOVE! I REALLY LOVE YOUR'S EVERYTHING!\"  \n\t\t";
+            Sleep(5000);
+        }
+        if (i == 98)
+        {
+            cout << "\n\n\t\t\tI    LOVE    YOU    KASIA   !    ";
+            cout << "\n\n\n\t\tWOULD YOU LIKE TO BE MY GIRLFRIEND????" << endl;
+            Sleep(5000);
+        }
+        if (i == 99)
+        {
+            for (j = 1; j <= 23; j++)
+            {
+                system("cls");
+                if (j == 1)
+                {
+                    cout << "\t\t *                   \n";
+                    Sleep(500);
+                }
+                if (j == 2)
+                {
+                    cout << "\t\t * .                 \n";
+                    Sleep(500);
+                }
+                if (j == 3)
+                {
+                    cout << "\t\t * .                  \n";
+                    cout << "\t\t      .                \n";
+                    Sleep(500);
+                }
+                if (j == 4)
+                {
+                    cout << "\t\t * .     .             \n";
+                    cout << "\t\t      .                \n";
+                    Sleep(500);
+                }
+                if (j == 5)
+                {
+                    cout << "\t\t * .     . *           \n";
+                    cout << "\t\t      .                \n";
+                    Sleep(500);
+                }
+                if (j == 6)
+                {
+                    cout << "\t\t * .     . * .           \n";
+                    cout << "\t\t      .                  \n";
+                    Sleep(500);
+                }
+                if (j == 7)
+                {
+                    cout << "\t\t * .     . * .           \n";
+                    cout << "\t\t      .      *           \n";
+                    Sleep(500);
+                }
+                if (j == 8)
+                {
+                    cout << "\t\t * .     . * .           \n";
+                    cout << "\t\t      .      *           \n";
+                    cout << "\t\t            *           \n";
+                    Sleep(500);
+                }
+                if (j == 9)
+                {
+                    cout << "\t\t * .     . * .            \n";
+                    cout << "\t\t      .      *           \n";
+                    cout << "\t\t            *           \n";
+                    cout << "\t\t           *            \n";
+                    Sleep(500);
+                }
+                if (j == 10)
+                {
+                    cout << "\t\t * .     . * .           \n";
+                    cout << "\t\t      .      *         \n";
+                    cout << "\t\t            *          \n";
+                    cout << "\t\t           *            \n";
+                    cout << "\t\t          *             \n";
+                    Sleep(500);
+                }
+                if (j == 11)
+                {
+                    cout << "\t\t * .     . * .           \n";
+                    cout << "\t\t      .      *          \n";
+                    cout << "\t\t            *          \n";
+                    cout << "\t\t           *            \n";
+                    cout << "\t\t          *             \n";
+                    cout << "\t\t         *               \n";
+                    Sleep(500);
+                }
+                if (j == 12)
+                {
+                    cout << "\t\t * .     . * .           \n";
+                    cout << "\t\t      .      *          \n";
+                    cout << "\t\t            *          \n";
+                    cout << "\t\t           *            \n";
+                    cout << "\t\t          *             \n";
+                    cout << "\t\t         *               \n";
+                    cout << "\t\t       *                 \n";
+                    Sleep(500);
+                }
+                if (j == 13)
+                {
+                    cout << "\t\t * .     . * .           \n";
+                    cout << "\t\t      .      *          \n";
+                    cout << "\t\t            *          \n";
+                    cout << "\t\t           *            \n";
+                    cout << "\t\t   *      *             \n";
+                    cout << "\t\t     *   *               \n";
+                    cout << "\t\t       *                 \n";
+                    Sleep(500);
+                }
+                if (j == 14)
+                {
+                    cout << "\t\t * .     . * .           \n";
+                    cout << "\t\t      .      *           \n";
+                    cout << "\t\t            *            \n";
+                    cout << "\t\t *         *             \n";
+                    cout << "\t\t   *      *              \n";
+                    cout << "\t\t     *   *               \n";
+                    cout << "\t\t       *                 \n";
+                    Sleep(500);
+                }
+                if (j == 15)
+                {
+                    cout << "\t\t * .     . * .          \n";
+                    cout << "\t\t      .      *          \n";
+                    cout << "\t\t*           *           \n";
+                    cout << "\t\t *         *            \n";
+                    cout << "\t\t   *      *             \n";
+                    cout << "\t\t     *   *              \n";
+                    cout << "\t\t       *                \n";
+                    Sleep(500);
+                }
+                if (j == 16)
+                {
+                    cout << "\t\t    . * .     . * .           \n";
+                    cout << "\t\t   *      .       *           \n";
+                    cout << "\t\t   *              *           \n";
+                    cout << "\t\t     *           *            \n";
+                    cout << "\t\t      *        *             \n";
+                    cout << "\t\t        *     *              \n";
+                    cout << "\t\t           *                 \n";
+                    Sleep(500);
+                }
+                if (j == 17)
+                {
+                    cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
+                    cout << "\t\t*     .       *                     *       .       *                 \n";
+                    cout << "\t\t*             *                      *              *                   \n";
+                    cout << "\t\t*    I       *     LOVE               *    YOU   *                    \n";
+                    cout << "\t\t  *        *                            *        *                     \n";
+                    cout << "\t\t    *    *                                *    *                      \n";
+                    cout << "\t\t       *                                     *                         \n";
+                    Sleep(500);
+                }
+                if (j == 18)
+                {
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t     I             LOVE                          YOU                           \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    Sleep(500) ;
+                }
+                if (j == 19)
+                {
+                    cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
+                    cout <<  "\t\t*     .       *                     *       .       *                 \n";
+                    cout << "\t\t*             *                      *              *                   \n";
+                    cout << "\t\t*   I        *     LOVE               *    YOU   *                    \n";
+                    cout << "\t\t  *        *                            *        *                     \n";
+                    cout << "\t\t    *    *                                *    *                      \n";
+                    cout << "\t\t       *                                     *                         \n";
+                    Sleep(500);
+                }
+                if (j == 20)
+                {
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t     I             LOVE                          YOU                           \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    Sleep(500) ;
+                }
+                if (j == 21)
+                {
+                    cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
+                    cout <<  "\t\t*     .       *                     *       .       *                 \n";
+                    cout << "\t\t*              *                     *              *                   \n";
+                    cout << "\t\t*   I        *     LOVE               *    YOU     *                    \n";
+                    cout << "\t\t  *        *                            *        *                     \n";
+                    cout << "\t\t    *    *                                *    *                      \n";
+                    cout << "\t\t       *                                     *                         \n";
+                    Sleep(500);
+                }
+                if (j == 22)
+                {
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t     I             LOVE                          YOU                           \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    cout << "\t\t                                                                       \n";
+                    Sleep(500) ;
+                }
+                if (j == 23)
+                {
+                    cout << "\t\t. * .     . * .                       . * .     . * .                   \n";
+                    cout <<  "\t\t*     .       *                     *       .       *                 \n";
+                    cout << "\t\t*              *                     *              *                   \n";
+                    cout << "\t\t*   I        *     LOVE               *    YOU   *                    \n";
+                    cout << "\t\t  *        *                            *        *                     \n";
+                    cout << "\t\t    *    *                                *    *                      \n";
+                    cout << "\t\t       *                                     *                         \n";
+                    Sleep(500);
+                }
+            }
+            Sleep(3000);
+        }
+        if (i == 100)
+        {
+            cout << "\n\n\t\t\"I love you... ::  %100 I love you!!!\"";
+            cout << "\n\n\n\t\t\t------------ \"I will always love you.\" --------------\n\n\n\n\n"<<endl;
+            Sleep(5000);
+        }
+        Sleep(100);
+    }
+    cout << "\n\n\n\n\t\t\t------\"Created by TAHA BURAK SAHIN.\"-------    "<< "\n\n\t\t\t\"Copyright belongs to TAHA.\"   \n\n\n "<<endl;
+    system("pause") ;
+}
